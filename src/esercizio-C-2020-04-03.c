9d2dd42bc38b81e575eb2557635f78864fe78966
dandreapaolo98 <dandrea.paolo1998@gmail.com>
2020-04-03 16:54:23 +0200

    Update esercizio-C-2020-04-03.c

--- src/esercizio-C-2020-04-03.c
+++ src/esercizio-C-2020-04-03.c
@@ -1,1 +1,121 @@
-//
+/*è dato il tipo dati contact_type, dovrebbe servire ad implementare una rubrica telefonica molto semplice:
+
+#define NAME_LEN 64
+#define PHONE_LEN 20
+
+typedef struct {
+   int id; // id numerico del contatto (valore univoco)
+   char name[NAME_LEN + 1]; // nome della persona
+   char phone[PHONE_LEN + 1]; // numero di telefono
+} contact_type;
+
+scrivere le seguenti funzioni:
+
+// crea un oggetto di tipo contact_type e lo inizializza con name e phone
+contact_type * create_contact(char * name, char * phone);
+
+// scrive su stdout i contenuti dell'oggetto
+void print_contact(contact_type * person);
+
+.....
+
+// utilizzare questa implementazione di main:
+
+int main(int argc, char *argv[]) {
+
+	contact_type * pino = create_contact("pino rossi", "+393331234567");
+
+	if (pino == NULL) {
+		printf("errore in create_contact!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	contact_type * gino = create_contact("gino verdi", "+393487654321");
+
+	if (gino == NULL) {
+		printf("errore in create_contact!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	print_contact(pino);
+
+	print_contact(gino);
+
+	return 0;
+}
+
+
+il risultato dell'esecuzione del programma sarà questo:
+
+person: id=0, name='pino rossi', phone='+393331234567'
+person: id=1, name='gino verdi', phone='+393487654321'*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NAME_LEN 64
+#define PHONE_LEN 20
+
+int count = 0;	//id incrementale per i contatti
+
+typedef struct {
+   int id; // id numerico del contatto (valore univoco)
+   char name[NAME_LEN + 1]; // nome della persona
+   char phone[PHONE_LEN + 1]; // numero di telefono
+} contact_type;
+
+
+// crea un oggetto di tipo contact_type e lo inizializza con name e phone
+contact_type * create_contact(char * name, char * phone);
+
+// scrive su stdout i contenuti dell'oggetto
+void print_contact(contact_type * person);
+
+int main(int argc, char *argv[]) {
+
+	contact_type * pino = create_contact("pino rossi", "+393331234567");
+
+	if (pino == NULL) {
+		printf("errore in create_contact!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	contact_type * gino = create_contact("gino verdi", "+393487654321");
+
+	if (gino == NULL) {
+		printf("errore in create_contact!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	print_contact(pino);
+
+	print_contact(gino);
+
+	return 0;
+}
+
+contact_type * create_contact(char * name, char * phone){
+	contact_type * new_contact;
+	contact_type temp;
+
+	new_contact = &temp;
+
+	new_contact->id = count;
+	strcpy(new_contact->name, name);
+	strcpy(new_contact->phone, phone);
+
+	count++;
+
+	return new_contact;
+}
+
+void print_contact(contact_type * person){
+	printf("\nInformazioni sul contatto:");
+
+	printf("\n\tid : %d", person->id);
+	printf("\n\tnome : %s", person->name);
+	printf("\n\tnumero di telefono : %s", person->phone);
+}
+
+
